5bb08077871c5ba82ae81d61d4682a8cdba09502
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-11-27 21:58:41 +0530

    Refactor code structure for improved readability and maintainability

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,18 @@
 
-Window.clearcolor = get_color_from_hex("#ffffff")
 
-#! Screen Manager
+#! import admin screens
+from screens.admin import (    #todo: admin panel for managing users
+    HomeScreen,
+    PatientListScreen,
+    PatientAddScreen,
+    DoctorListScreen,
+    DoctorAddScreen
+)
+
+
+#! Output Screen Example: Mobile screen size (iPhone 14 approx)
+#Window.size = (490, 600)  #! Width x Height in pixels
+
+# Window.size = (250, 540)  #! Width x Height in pixels
+
+#! Register custom widgets from kivy_garden
